aee0220728162e791096c35c08e0c321540d3778
aj <leraalfredojohn@gmail.com>
2023-10-02 10:36:58 +0800

    CORS

--- index.js
+++ index.js
@@ -10,27 +10,21 @@ const postRoute = require("./routes/postRoute");
 // ENV
 const BASE = process.env.BASE_URL;
 const RENDER = process.env.RENDER_URL;
-const NETLIFY = process.env.NETLIFY_URL;
 const MONGODB_URL = process.env.MONGODB_URL;
 const PORT = process.env.PORT;
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Replace '*' with your trusted origin(s).
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
 
-// Enable CORS for specific origins (e.g., your Netlify site)
-const allowedOrigins = [RENDER, BASE, NETLIFY];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true, // Enable credentials (cookies)
-};
-
-app.use(cors(corsOptions));
-
-// app.use(cors({ credentials: true, origin: [BASE, RENDER] }));
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+app.use(cors({ credentials: true, origin: [BASE, RENDER] }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
