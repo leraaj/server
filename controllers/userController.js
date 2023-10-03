b2bb5a4abf89a641a072ce65003ac8fed464dcf5
aj <leraalfredojohn@gmail.com>
2023-10-03 12:04:54 +0800

    New commits

--- controllers/userController.js
+++ controllers/userController.js
@@ -81,8 +81,13 @@ const deleteUser = async (request, response) => {
 };
 const login = async (request, response) => {
   try {
-    const inputUsername = "admin"; //request.body.username;
-    const inputPassword = "admin"; //request.body.password;
+    var userToken = createToken("65187726c1c60f48e044194f");
+    response.cookie("Auth_Token", userToken, {
+      httpOnly: true,
+      maxAge: cookieExpires,
+    });
+    const inputUsername = request.body.username;
+    const inputPassword = request.body.password;
     // Check if either username or password is empty
     if (!inputUsername || !inputPassword) {
       return response
